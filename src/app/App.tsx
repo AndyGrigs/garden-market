2446efe0d7330bc2b30d56f771b85dcbf6e90190
Andriy <andygrigs88@gmail.com>
2026-01-15 23:48:43 +0100

    Add App and AnimatedRoutes components for application structure and routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
-import AuthLoader from './components/AuthLoader';
+import { store } from '../store/store';
 import { Toaster } from 'react-hot-toast';
+import AuthLoader from '../components/AuthLoader';
 
 function App() {
 
